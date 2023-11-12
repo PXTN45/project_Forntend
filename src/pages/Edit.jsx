2a194793617731fdefa2b429caf36c96d97b1243
PXTN2 <150564535+PXTN2@users.noreply.github.com>
2023-11-12 18:31:10 +0700

    add button cancel in edit.jsx

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 import { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -21,7 +21,7 @@ const config = {
     username: USERNAME,
     password: PASSWORD,
   },
-    // headers : authHeader(),
+  // headers : authHeader(),
 };
 
 const theme = createTheme({
@@ -42,118 +42,127 @@ const theme = createTheme({
 });
 
 export default function Edit() {
-    const [restaurant, setRestaurant] = useState({
-        name: "",
-        type: "",
-        img: "",
-      });
-      const navigate = useNavigate();
-      const [error, setError] = useState(false);
-      const {restaurantId} = useParams();
-      const handelChange = (e) => {
-        setRestaurant((prev)=>({...prev, [e.target.name]:e.target.value}));
+  const [restaurant, setRestaurant] = useState({
+    name: "",
+    type: "",
+    img: "",
+  });
+  const navigate = useNavigate();
+  const [error, setError] = useState(false);
+  const { restaurantId } = useParams();
+  const handelChange = (e) => {
+    setRestaurant((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  useEffect(() => {
+    const fetchAllRestaurants = async () => {
+      try {
+        const res = await axios.get(`${URL}/restaurant/${restaurantId}`);
+        setRestaurant(res.data.result);
+      } catch (error) {
+        console.error(error);
       }
-      useEffect(() => {
-        const fetchAllRestaurants = async () => {
-          try {
-            const res = await axios.get(`${URL}/restaurant/${restaurantId}`);
-            setRestaurant(res.data.result);
-          } catch (error) {
-            console.error(error);
-          }
-        };
-        fetchAllRestaurants();
-      }, [restaurantId]);
+    };
+    fetchAllRestaurants();
+  }, [restaurantId]);
 
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-          await axios.put(`${URL}/restaurant/${restaurantId}`,restaurant, config);
-          navigate("/")
-        } catch (error) {
-          console.error(error);
-          setError(true);
-        }
-      }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.put(`${URL}/restaurant/${restaurantId}`, restaurant, config);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      setError(true);
+    }
+  };
 
-      const handleClear = () => {
-        setRestaurant({
-          name: "",
-          type: "",
-          img: "",
-        });
-        setError(false);
-      }
-      return (
-        <ThemeProvider theme={theme}>
-          <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <Box
-              sx={{
-                marginTop: 8,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
+  const handleCancel = () => {
+    setRestaurant({
+      name: "",
+      type: "",
+      img: "",
+    });
+    setError(false);
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5" marginTop={15}>
+            Edit Restaurant
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="name"
+              onChange={handelChange}
+              value={restaurant.name}
+              name="name"
+              autoComplete="name"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="type"
+              label="type"
+              onChange={handelChange}
+              value={restaurant.type}
+              type="type"
+              id="type"
+              autoComplete="current-type"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="img"
+              label="img"
+              onChange={handelChange}
+              value={restaurant.img}
+              type="img"
+              id="img"
+              autoComplete="current-type"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
             >
-              <Typography component="h1" variant="h5" marginTop={15}>
-                Edit Restaurant
-              </Typography>
-              <Box
-                component="form"
-                onSubmit={handleSubmit}
-                noValidate
-                sx={{ mt: 1 }}
-              >
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="name"
-                  label="name"
-                  onChange={handelChange}
-                  value={restaurant.name}
-                  name="name"
-                  autoComplete="name"
-                  autoFocus
-                />
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="type"
-                  label="type"
-                  onChange={handelChange}
-                  value={restaurant.type}
-                  type="type"
-                  id="type"
-                  autoComplete="current-type"
-                />
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="img"
-                  label="img"
-                  onChange={handelChange}
-                  value={restaurant.img}
-                  type="img"
-                  id="img"
-                  autoComplete="current-type"
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Edit
-                </Button>
-              </Box>
-            </Box>
-          </Container>
-        </ThemeProvider>
-      );
+              Edit
+            </Button>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="secondary"
+              onClick={handleCancel}
+            >
+              cancel
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 // export default Edit
