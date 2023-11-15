b9c4fd18fb8d3e265beb595481a60b5f120acad2
PXTN2 <150564535+PXTN2@users.noreply.github.com>
2023-11-15 16:25:22 +0700

    update profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,29 +3,30 @@ import { useAuthContext } from "../context/auth.context";
 export const Profile = () => {
   const { user } = useAuthContext();
   return (
-    <div className="container" id="profile">
+    <div className="container" id="profile" >
       <div className="row">
         <div
           className="card profile my-2 light" 
-          style={{ width: "500px", borderRadius: "10px" }}
+          style={{ width: "500px", borderRadius: "10px", margin:"5cm"}}
         >
-          <div className="card-header"style={{ backgroundColor: "#ef6c00" }}> Profile </div>
+          <div className="card-header"style={{ backgroundColor: "#ef6c00",color:"#fff" }}> Profile </div>
           <div className="card-body">
+            <img src="https://cdn.blerp.com/thumbnails/10132e40-218a-11ee-8c08-5128a26885f3" alt="user image" style={{  height:"10rem",width:"10rem", borderRadius:"100% "}}/>
+            <br/>
             <div className="card-title h5">{user.username} </div>
-            <div className="card-text">
+            {/* <div className="card-text">
               <b>Token</b>
               {user.accessToken.substring(0, 20)}...
               {user.accessToken.substring(user.accessToken.lenght - 20)}
-            </div>
+            </div> */}
             <div className="card-text">
               <b> Id: </b> {user.id}
               <br />
               <b> email: </b> {user.email}
-              <br />
-              <b> roles: </b> {user.roles.lenght}
+              <br/>
+               <p>{user.roles}</p>
               <ul>
-                {user.roles &&
-                  user.roles.map((role, index) => <ul key={index}>{role}</ul>)}
+               
               </ul>
             </div>
           </div>
